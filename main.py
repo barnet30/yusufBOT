b577fa165031c840a44797c26611967a10249f20
Danila <danil-efimov2000mag@mail.ru>
2020-09-29 23:24:53 +0300

    pushfile

--- main.py
+++ main.py
@@ -14,4 +14,5 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 print("Eaze")
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-print("hello aid" )
+print("hello aid")
+print("write shit")
