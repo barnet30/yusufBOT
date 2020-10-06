efd60db7fc3bcdadcf6737b96280155a33e32d9a
Danila <danil-efimov2000mag@mail.ru>
2020-10-06 13:06:29 +0300

    ubpdate

--- echobot.py
+++ echobot.py
@@ -1,19 +1,24 @@
 import telebot
 
 bot = telebot.TeleBot("1301833528:AAHzcF_MANvxQuzssdKLJfNbRJMHp1D8AJ4", parse_mode=None)
-user = bot.get_me()
 
 
-@bot.message_handler(commands=['start','help'])
+@bot.message_handler(commands=['start'])
 def set_welcome(message):
-    bot.reply_to(message, "Bros, how are you?")
+    bot.reply_to(message, "Hi, how can i help you?")
 
-@bot.message_handler(func= lambda x:True)
-def echo_all(message):
-    bot.reply_to(message,message.text)
+@bot.message_handler(commands=['hello'])
+def set_welcome(message):
+    bot.reply_to(message, "Приветствую тебя в мире cars")
 
-@bot.callback_query_handler(func= lambda x:True)
-def test_callback(call):
-    user.info(call)
+# @bot.message_handler(func= lambda x:True)
+# def echo_all(message):
+#     bot.reply_to(message,message.text)
+
+@bot.message_handler(content_types=['text'])
+def message_all(msg):
+    message = msg.text
+    user_id = msg.chat.id
+    bot.send_message(user_id, f"Вы написали {message}")
 
 bot.polling()
